1ffed17ae9197862ed0c2ef8ae1d2bc26e87728e
suviano <marcossilvano496@gmail.com>
2018-05-24 22:13:02 -0300

    FIX circle ci file

--- .circleci/test.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-curl --user ${CIRCLE_TOKEN}: \
-    --request POST \
-    --form revision=${COMMIT_HASH} \
-    --form config=@config.yml \
-    --form notify=false \
-        https://circleci.com/api/v1.1/project/github/suviano/A-Kong-Gateway/tree/master
